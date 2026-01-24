7d2977fe125075b8db680be750925bfd7aa8e0e8
Ben Smith <btsmith.nz@gmail.com>
2026-01-25 02:08:15 +1300

    Fix auth config

--- convex/auth.ts
+++ convex/auth.ts
@@ -6,19 +6,13 @@ import { mutation, query } from "./_generated/server";
 import { betterAuth } from "better-auth";
 import authConfig from "./auth.config";
 
-const vercelUrl =
-  process.env.VERCEL_ENV === "production"
-    ? process.env.VERCEL_PROJECT_PRODUCTION_URL!
-    : process.env.VERCEL_BRANCH_URL!;
-const siteUrl = vercelUrl || "http://localhost:3000";
-
 // The component client has methods needed for integrating Convex with Better Auth,
 // as well as helper methods for general use.
 export const authComponent = createClient<DataModel>(components.betterAuth);
 
 export const createAuth = (ctx: GenericCtx<DataModel>) => {
   return betterAuth({
-    baseURL: siteUrl,
+    baseURL: process.env.SITE_URL,
     database: authComponent.adapter(ctx),
     // Configure simple, non-verified email/password to get started
     emailAndPassword: {
